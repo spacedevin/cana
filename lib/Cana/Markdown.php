5401f7b2b167ec2b8d5e25fbe8dc317fc9c676b9
arzynik <github@arzynik.com>
2012-04-24 10:58:00 -0700

    updated markdown

--- lib/Cana/Markdown.php
+++ lib/Cana/Markdown.php
@@ -10,25 +10,12 @@
 
 
 // auto include the markdown parser
-$md = new Cana_Markdown_PHPMarkdown;
+$md = new Cana_Markdown_Markdown;
 
 class Cana_Markdown extends Cana_Model {
 
 	public static function parse($text) {
-
-		// Setup static parser variable.
-		static $parser;
-		if (!isset($parser)) {
-			$parser_class = MARKDOWN_PARSER_CLASS;
-			$parser = new $parser_class;
-		}
-	
-
-		// Transform text using parser.
-		$text = $parser->transform($text);
-//		$text = str_replace('<code>','<pre class="prettyprint linenums">',trim($text));
-		$text = str_replace("\n</code>",'</code>',trim($text));
-		$text = str_replace('<pre>','<pre class="prettyprint linenums">',trim($text));
+		$text = Markdown($text);
 
 		return $text;
 	}
